cb331eda00405f1bab7af44967994b09686509db
Terry Howe <terrylhowe@gmail.com>
2014-01-06 09:31:01 -0700

    dxcli-1121 health monitor

---
+++ openstackclient/network/v2_0/lb/healthmonitor.py
@@ -0,0 +1,102 @@
+#   Copyright 2012-2013 OpenStack, LLC.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+"""Load Balancer health monitor action implementations"""
+
+from neutronclient.neutron.v2_0.lb import healthmonitor as neu2
+from openstackclient.network import v2_0 as v2_0
+
+
+class CreateHealthMonitor(v2_0.CreateCommand):
+    """Create a load balancer health monitor"""
+
+    clazz = neu2.CreateHealthMonitor
+
+    def get_parser(self, prog_name):
+        parser = super(CreateHealthMonitor, self).get_parser(prog_name)
+        parser.add_argument(
+            '--admin-state-down',
+            dest='admin_state', action='store_false',
+            help='Set admin state up to false')
+        parser.add_argument(
+            '--expected-codes',
+            help='The list of HTTP status codes expected in '
+                    'response from the member to declare it healthy. This '
+                    'attribute can contain one value, '
+                    'or a list of values separated by comma, '
+                    'or a range of values (e.g. "200-299"). If this attribute '
+                    'is not specified, it defaults to "200". ')
+        parser.add_argument(
+            '--http-method',
+            help='The HTTP method used for requests by the monitor of type '
+                    'HTTP.')
+        parser.add_argument(
+            '--url-path',
+            help='The HTTP path used in the HTTP request used by the monitor'
+                    ' to test a member health. This must be a string '
+                    'beginning with a / (forward slash)')
+        parser.add_argument(
+            '--delay',
+            required=True,
+            help='The time in seconds between sending probes to members.')
+        parser.add_argument(
+            '--max-retries',
+            required=True,
+            help='Number of permissible connection failures before changing '
+                    'the member status to INACTIVE. [1..10]')
+        parser.add_argument(
+            '--timeout',
+            required=True,
+            help='Maximum number of seconds for a monitor to wait for a '
+                    'connection to be established before it times out. The '
+                    'value must be less than the delay value.')
+        parser.add_argument(
+            '--type',
+            required=True, choices=['PING', 'TCP', 'HTTP', 'HTTPS'],
+            help='One of predefined health monitor types')
+        return parser
+
+
+class DeleteHealthMonitor(v2_0.DeleteCommand):
+    """Delete a load balancer health monitor"""
+
+    clazz = neu2.DeleteHealthMonitor
+    name = 'id'
+    metavar = '<healthmonitor>'
+    help_text = 'Name or ID of load balancer health monitor to delete'
+
+
+class ListHealthMonitor(v2_0.ListCommand):
+    """List load balancer health monitor"""
+
+    clazz = neu2.ListHealthMonitor
+
+
+class SetHealthMonitor(v2_0.SetCommand):
+    """Set load balancer health monitor values"""
+
+    clazz = neu2.UpdateHealthMonitor
+    name = 'id'
+    metavar = '<healthmonitor>'
+    help_text = 'Name or ID of load balancer health monitor to update'
+
+
+class ShowHealthMonitor(v2_0.ShowCommand):
+    """Show a load balancer health monitor"""
+
+    clazz = neu2.ShowHealthMonitor
+    name = 'id'
+    metavar = '<healthmonitor>'
+    help_text = 'Name or ID of health monitor to show'
---
+++ openstackclient/tests/network/v2_0/lb/test_healthmonitor_integration.py
@@ -0,0 +1,148 @@
+#   Copyright 2013 OpenStack, LLC.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import httpretty
+
+from openstackclient.network.v2_0.lb import healthmonitor
+from openstackclient.tests.network.v2_0 import common
+
+
+class TestHealthMonitorIntegration(common.TestIntegrationBase):
+    HOSTESS = common.TestIntegrationBase.HOST + common.TestIntegrationBase.VER
+    POOLS_URL = HOSTESS + "/lb/pools.json"
+    POOLS = '{ "pools": [{ "id": "1111111" }] }'
+    SUBNETS_URL = HOSTESS + "/subnets.json"
+    SUBNETS = '{ "subnets": [{ "id": "222222" }] }'
+    CREATE_URL = HOSTESS + "/lb/health_monitors.json"
+    CREATE = """
+{
+   "health_monitor":
+   {
+       "status": "ACTIVE",
+       "name": "nameo",
+       "tenant_id": "33a40233",
+       "id": "a9254bdb"
+   }
+}"""
+    DELETE_URL = HOSTESS + "/lb/health_monitors/a9254bdb.json"
+    DELETE = "{}"
+    LIST_URL = HOSTESS + "/lb/health_monitors.json"
+    LIST_ONE = '{ "health_monitors": [{ "id": "a9254bdb" }] }'
+    LIST = """
+{
+   "health_monitors": [
+       {
+          "admin_state_up": "True",
+          "type": "PING",
+          "tenant_id": "33a40233",
+          "id": "a9254bdb"
+       },
+       {
+          "admin_state_up": "False",
+          "type": "HTTP",
+          "tenant_id": "33a40233",
+          "id": "b8408dgd"
+       }
+   ]
+}"""
+    SHOW_URL = HOSTESS + "/lb/health_monitors/a9254bdb.json"
+    SHOW = CREATE
+    ADD_REMOVE_URL = HOSTESS + "/floatingips/127.0.0.1.json"
+    ADD_REMOVE = "{}"
+
+    @httpretty.activate
+    def test_create(self):
+        pargs = common.FakeParsedArgs()
+        pargs.admin_state = True
+        pargs.expected_codes = None
+        pargs.http_method = None
+        pargs.url_path = None
+        pargs.delay = '30'
+        pargs.max_retries = '10'
+        pargs.timeout = '80'
+        pargs.type = 'PING'
+        pargs.tenant_id = '33a40233'
+        #httpretty.register_uri(httpretty.GET, self.POOLS_URL,
+        #                       body=self.POOLS)
+        #httpretty.register_uri(httpretty.GET, self.SUBNETS_URL,
+        #                       body=self.SUBNETS)
+        httpretty.register_uri(httpretty.POST, self.CREATE_URL,
+                               body=self.CREATE)
+        self.when_run(healthmonitor.CreateHealthMonitor, pargs)
+        self.assertEqual('', self.stderr())
+        self.assertEqual(u"""\
+Created a new health_monitor:
+id="a9254bdb"
+name="nameo"
+status="ACTIVE"
+tenant_id="33a40233"
+""", self.stdout())
+
+    @httpretty.activate
+    def test_delete(self):
+        pargs = common.FakeParsedArgs()
+        pargs.id = 'nameo'
+        httpretty.register_uri(httpretty.GET, self.LIST_URL,
+                               body=self.LIST_ONE)
+        httpretty.register_uri(httpretty.DELETE, self.DELETE_URL,
+                               body=self.DELETE)
+        self.when_run(healthmonitor.DeleteHealthMonitor, pargs)
+        self.assertEqual('', self.stderr())
+        self.assertEqual(u'Deleted health_monitor: nameo\n',
+                         self.stdout())
+
+    @httpretty.activate
+    def test_list(self):
+        pargs = common.FakeParsedArgs()
+        pargs.page_size = None
+        pargs.sort_key = []
+        pargs.sort_dir = []
+        pargs.formatter = 'csv'
+        httpretty.register_uri(httpretty.GET, self.LIST_URL,
+                               body=self.LIST)
+        self.when_run(healthmonitor.ListHealthMonitor, pargs)
+        self.assertEqual('', self.stderr())
+        self.assertEqual("""\
+id,type,admin_state_up
+a9254bdb,PING,True
+b8408dgd,HTTP,False
+""", self.stdout())
+
+    @httpretty.activate
+    def test_set(self):
+        pargs = common.FakeParsedArgs()
+        pargs.id = 'nameo'
+        httpretty.register_uri(httpretty.GET, self.LIST_URL,
+                               body=self.LIST_ONE)
+        self.when_run(healthmonitor.SetHealthMonitor, pargs)
+        self.assertEqual('', self.stderr())
+        self.assertEqual('', self.stdout())
+
+    @httpretty.activate
+    def test_show(self):
+        pargs = common.FakeParsedArgs()
+        pargs.id = 'nameo'
+        httpretty.register_uri(httpretty.GET, self.LIST_URL,
+                               body=self.LIST_ONE)
+        httpretty.register_uri(httpretty.GET, self.SHOW_URL,
+                               body=self.SHOW)
+        self.when_run(healthmonitor.ShowHealthMonitor, pargs)
+        self.assertEqual('', self.stderr())
+        self.assertEqual(u"""\
+id="a9254bdb"
+name="nameo"
+status="ACTIVE"
+tenant_id="33a40233"
+""", self.stdout())
